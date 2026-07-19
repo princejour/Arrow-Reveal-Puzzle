d7a434b50b3e627a83293a96f9abaa6a9e56e957
princejour <61734521+princejour@users.noreply.github.com>
2026-07-19 17:45:31 +0100

    Create ImagePattern enum for puzzle images
    Added an enum class for image patterns used in the puzzle.

---
+++ app/src/main/java/com/arrowreveal/puzzle/model/ImagePattern.kt
@@ -0,0 +1,27 @@
+package com.arrowreveal.puzzle.model
+
+
+/**
+ * قوالب تشكيل الأسهم
+ *
+ * كل قالب يمثل صورة مخفية
+ * من خلال توزيع القطع داخل الشبكة.
+ */
+enum class ImagePattern {
+
+
+    LION,
+
+
+    CAT,
+
+
+    WOMAN,
+
+
+    MAP,
+
+
+    EAGLE
+
+}
